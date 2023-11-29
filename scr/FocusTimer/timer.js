6dfa4f80bb0d498f904061a3664ca1ea088316c1
Diego Gomes <diegomes000@gmail.com>
2023-11-28 21:25:00 -0300

    Add files via upload

---
+++ scr/FocusTimer/timer.js
@@ -0,0 +1,47 @@
+import state from './state.js'
+import * as el from './elements.js'
+import { resetTimer } from './actions.js'
+import { kitchenTimer } from './sounds.js'
+
+export function countdown() {
+
+    clearTimeout(state.countdownId)
+
+    if(!state.isRunning) {
+        return
+    }
+
+    let minutes = Number(el.minutes.textContent)
+    let seconds = Number(el.seconds.textContent)
+
+    seconds--
+    if(seconds < 0) {
+        seconds = 59
+        minutes--   
+    }
+
+    if(minutes < 0) {
+        resetTimer()
+        kitchenTimer.play()
+        return
+    }
+
+    updateDisplay(minutes,seconds)
+
+    state.countdownId = setTimeout(() => countdown(), 1000)
+    /* RECURSAO setTimeout com a propria function executada repetidamente até 1000ms - if !isrunning - return
+    countdownId zerar*/
+
+}
+
+export function updateDisplay(minutes, seconds) {
+    minutes = minutes ?? state.minutes
+    /* nullish operator */
+    seconds = seconds ?? state.seconds
+    
+    el.minutes.textContent = String(minutes).padStart(2, "0")
+    el.seconds.textContent = String(seconds).padStart(2, "0")
+
+
+
+}
